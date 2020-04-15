14c8d9308e9d8d1f2f8a4989fee49a285b0e317e
Rutwik Bharadwaj <rutwik.bharadwaj@nike.com>
2020-04-15 14:32:23 -0700

    save

---
+++ aws-commands.sh
@@ -0,0 +1,3 @@
+aws ec2 stop-instances --instance-ids i-0a446624639323ed8 --profile NIKE.SSO.AdminRole --region us-east-1
+aws ec2 start-instances --instance-ids i-0a446624639323ed8 --profile NIKE.SSO.AdminRole --region us-east-1
+aws ec2 describe-instances --instance-ids i-0a446624639323ed8 --profile NIKE.SSO.AdminRole --region us-east-1
